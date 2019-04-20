f66e0de47dfc4253645ab727d13668cee2e2cd63
huanglp <huanglongpeng@talkmoney.cn>
2019-04-20 10:31:32 +0800

    some change

--- CampusTradingPlatform/CampusTradingPlatform-part05-controller/src/main/webapp/js/controller/userController.js
+++ CampusTradingPlatform/CampusTradingPlatform-part05-controller/src/main/webapp/js/controller/userController.js
@@ -4,6 +4,13 @@ app.controller("userController", function ($scope, userService, itemService) {
     //注册
     $scope.register = function () {
         userService.reg($scope.user).success(function (result) {
+            var btnArray = ['确定'];
+
+            mui.confirm('确定下架该商品?', '警告', btnArray, function (e) {
+                if (e.index == 0) {
+                } else {
+                }
+            })
             $scope.user = null;
             location.href = "index.html";
         })
